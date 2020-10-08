f837a26125a99ec6bf21f9d2b4d3e9dda74ed3a5
AviFS <58723244+AviFS@users.noreply.github.com>
2020-10-07 20:36:07 -0700

    Update A4-2.js

--- html/js/A4-2.js
+++ html/js/A4-2.js
@@ -1,6 +1,6 @@
-var carDoor, chosenDoor, reveal, switched;
+// Side-Effects
 
-// Global
+var carDoor, chosenDoor, reveal, switched;
 
 //// On Load
 function startMonty() {
@@ -25,10 +25,8 @@ function chooseDoor(picked) {
 
 //// Small Tasks
 function calcRevealDoor() {
-    // IF CHOSE GOAT → RETURN OTHER GOAT
     if (chosenDoor != carDoor)
         return remainingDoor(carDoor, chosenDoor);
-    // IF CHOSE CAR → RETURN RANDOM GOAT
     var otherDoors = [mod(carDoor-1,3), mod(carDoor+1,3)];
     return otherDoors[randInt(2)];
 }
@@ -41,11 +39,13 @@ function askSwitch() {
 
 function checkWin() {
     if (chosenDoor==carDoor) {
-        document.getElementById('display').innerHTML = "Congratulations, you won the car. " + "Must've made the right choice " + (switched? "switching!" : "staying!");
+        document.getElementById('display').innerHTML = "Congratulations, you won the car. " +
+            "Must've made the right choice " + (switched? "switching!" : "staying!");
             setDoor(chosenDoor, 'car');
         }
     else {
-        document.getElementById('display').innerHTML = "Darn, you got a goat! " + "Maybe you should've " + (switched? "stayed!" : "switched!");
+        document.getElementById('display').innerHTML = "Darn, you got a goat! " +
+        "Maybe you should've " + (switched? "stayed!" : "switched!");
         setDoor(chosenDoor, 'goat');
     }
 }
@@ -61,7 +61,7 @@ function playAgain() {
     },500);
 }
 
-// Functional
+// Pure
 
 //// Monty
 function remainingDoor(door1, door2) {
@@ -69,13 +69,6 @@ function remainingDoor(door1, door2) {
 }
 
 function setDoor(n, img) {
-    // EXAMPLES
-    //// → setDoor(2, 'goat')
-    //// → document.getElementById('door2').src = 'goat-door.png'
-
-    ///// → setDoor(0, 'closed')
-    ///// → document.getElementById('door0').src = 'closed-door.png'
-
     document.getElementById('door'+n.toString()).src = img+'-door.png'
 }
 
@@ -93,21 +86,3 @@ function randInt(n) {
 function mod(n,k) {
     return ((n%k)+k)%k;
 }
-
-// Why I wrote mod myself
-// -1%3 → -1
-//%3 → 2
-
-
-// calcRevealDoor()
-
-// DDD    DDD      DDD
-//  C      G        G
-
-//  ^
-
-
-// var otherDoors
-//// 2→[0,1]
-//// 1→[0,2]
-//// 0→[1,2]
